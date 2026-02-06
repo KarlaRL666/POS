bf1441abe1d24173ee04ff62ac0055d7c05e6257
Karla <karlamartinezl2004@gmail.com>
2026-02-06 14:35:32 -0600

    Colocacion de botones de navegacion en la clase container.py

--- container.py
+++ container.py
@@ -31,5 +31,43 @@ class Container(tk.Frame):
         frame= self.frames[container]
         frame.tkraise()
     
+    def ventas(self):
+        self.show_frames(Ventas)
+    
+    def inventario(self):
+        self.show_frames(Inventario)
+    
+    def clientes(self):
+        self.show_frames(Clientes)
+    
+    def pedidos(self):
+        self.show_frames(Pedidos)
+    
+    def proveedor(self):
+        self.show_frames(Proveedor)
+        
+    def informacion(self):
+        self.show_frames(Informacion)
+        
     def widgets(self):
+        frame2 = tk.Frame(self)
+        frame2.place(x=0, y=0, width=1100, height=40)
+        
+        self.btn_ventas = Button(frame2, fg='black', text='Ventas', font='sans 16 bold', command=self.ventas)
+        self.btn_ventas.place(x=0, y=0, width=184, height=40)
+        
+        self.btn_inventario = Button(frame2, fg='black', text='Inventario', font='sans 16 bold', command=self.inventario)
+        self.btn_inventario.place(x=184, y=0, width=184, height=40)
+        
+        self.btn_clientes = Button(frame2, fg='black', text='Clientes', font='sans 16 bold', command=self.clientes)
+        self.btn_clientes.place(x=369, y=0, width=184, height=40)
+        
+        self.btn_pedidos = Button(frame2, fg='black', text='Pedidos', font='sans 16 bold', command=self.pedidos)
+        self.btn_pedidos.place(x=554, y=0, width=184, height=40)
+        
+        self.btn_proveedor = Button(frame2, fg='black', text='Proveedor', font='sans 16 bold', command=self.proveedor)
+        self.btn_proveedor.place(x=739, y=0, width=184, height=40)
+        
+        self.btn_informacion = Button(frame2, fg='black', text='Informacion', font='sans 16 bold', command=self.informacion)
+        self.btn_informacion.place(x=923, y=0, width=184, height=40)
         pass        

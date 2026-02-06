3ebd68c7fa01d818433eeca7a0c064d0cc806628
Karla <karlamartinezl2004@gmail.com>
2026-02-06 13:38:00 -0600

    Creacion de clase manager como clase principal

---
+++ manager.py
@@ -0,0 +1,41 @@
+from tkinter import *
+from tkinter import ttk
+from login import Login
+from container import Container
+import sys
+import os 
+
+class Manager(Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.title('Punto de Venta')
+        self.geometry('1100x650+120+20')
+        self.resizable(False, False)
+        
+        container =Frame(self)
+        container.pack(side=TOP, fill=BOTH, expand=True)
+        container.configure(bg='#C6D9E3')
+        
+        self.frame={}
+        for i in (Login, Registro, Container):
+            frame = i(container, self)
+            self.frames[i]=frame
+            
+        self.show_frame(Login)
+        
+        # declaracion del estilo a usar
+        self.style = ttk.Style()
+        self.style.theme_use('clam')
+        
+    def show_frame(self, container):
+        frame = self.frame[container]
+        
+        # se usa para llamar al frente
+        frame.tkraise()
+
+def main():
+    app = Manager()
+    app.mainloop()
+    
+if __name__ == '__main__':
+    main()
